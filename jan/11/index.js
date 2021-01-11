599bfb1f5355e1265b224ff70e24d44a2ae3da34
Malte Podolski <malte.podolski@web.de>
2021-01-11 15:51:29 +0100

    Add assignments

---
+++ jan/11/index.js
@@ -0,0 +1,52 @@
+
+// const names = ['sid', 'nancy', 'paul'];
+
+// for (let i = 0; i < names.length; i++) {
+
+//   str = names[i][0].toUpperCase() + names[i].substring(1);
+
+//   console.log(str);
+// }
+
+
+// for (let i = 0; i <= 20; i++) {
+
+//   let number = i % 2 === 1 ? 'odd' : 'even';
+
+//   console.log(i, ' is ', number);
+// }
+
+
+// 100 200 300 400 500 600 700 800 900 1000
+// for (let i = 100; i <= 1000; i += 100) {
+//   console.log(i);
+// }
+// 0 2 4 6 8 10
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
+// 3 6 9 12 15
+// for (let i = 3; i <= 15; i += 3) {
+  // console.log(i);
+// }
+// 9 8 7 6 5 4 3 2 1 0
+// for (let i = 9; i >= 0; i--) {
+//   console.log(i);
+// }
+// 1 1 1 2 2 2 3 3 3 4 4 4
+// for (let i = 1; i <= 4; i++) {
+//   console.log(i, i, i);
+// }
+// 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4
+for (let i = 0; i <= 2; i++) {
+  for (let i = 0; i <= 4; i++) {
+    console.log(i);
+  }
+}
+// 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4
+for (let i = 1; i <= 4; i++) {
+  let str;
+  for (let j = 1; j <= 4; j++) {
+    console.log(i);
+  }
+}
